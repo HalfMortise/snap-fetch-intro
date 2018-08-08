3d1f8bbdc696f72d3d989cee27f6ca3f6acf8e9c
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-08 13:14:59 -0600

    Created .js and added instructions

---
+++ fetch.js
@@ -0,0 +1,7 @@
+/**
+ *Instructions:
+ * Use fetch method in JavaScript to make an API call to https://jsonplaceholder.typicode.com/posts and console.log the results
+ * Extra credit: display the results of the API call on the DOM
+ * You must create a deployment on bootcamp-coders for this assignment
+ */
+
