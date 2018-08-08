d0cd759ecb97f3d51f2a22afc45408f0f5b402c9
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-08 13:27:33 -0600

    Trying another version

--- fetch.js
+++ fetch.js
@@ -6,19 +6,4 @@
  */
 
 fetch('https://jsonplaceholder.typicode.com/posts')
-	.then(
-		function(response) {
-			if (response.status !==200) {
-				console.log('Dont quite know what to say here' + response.status);
-				return;
-			}
-
-			//examine the text in the response
-			response.json().then(function(data) {
-				console.log(data);
-			});
-		}
-	)
-	.catch(function(err) {
-		console.log('Fetch Error :-S', err);
-});
+	.then(res => console.log(res));
